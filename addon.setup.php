2d6cd343a444595047ecf38aa768a5053816b7c0
stefcab <stefcab@gmail.com>
2018-10-18 00:12:03 -0300

    Create addon.setup.php
    Need addon.setup.php for the  EE4 plugin structure file.

---
+++ addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'author'      => 'Pierre-Vincent Ledoux',
+	'author_url'  => 'http://twitter.com/pvledoux/',
+	'name'        => 'Pvl Increment',
+	'description' => 'Auto-increment on each call in the same template',
+	'version'     => '1.0',
+	'namespace'   => 'pvl_increment'
+	);
